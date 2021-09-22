d509452d7d2f0c97b80c4db105720bb8b8df01c6
Chris Curvey <ccurvey@transparint.com>
2021-09-22 10:20:18 -0400

    corrected username

--- test_project/settings.py
+++ test_project/settings.py
@@ -13,7 +13,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'test_jsonate',
-        'USER': 'postrgres',
+        'USER': 'postgres',
     }
 }
 
