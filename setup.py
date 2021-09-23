796ea6626d325a2fff635cb9bc8087a4f21bb30c
Chris Curvey <ccurvey@transparint.com>
2021-09-23 08:06:00 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -2,31 +2,31 @@ from setuptools import setup
 
 setup(
     name='jsonate',
-    version='0.7.1',
-    
+    version='0.7.3',
+
     author='James Robert',
     author_email='jiaaro@gmail.com',
-    
+
     description=('Django library that can make ANYTHING into json'),
     long_description=open('README.markdown').read(),
     long_description_content_type="text/markdown",
-    
+
     license='MIT',
     keywords='django json templatetags',
-    
+
     url='http://jsonate.com',
-    
+
     install_requires=[
         "django>=3.0",
     ],
-    
+
     packages=[
-        'jsonate', 
+        'jsonate',
         'jsonate.templatetags',
     ],
-    
+
     include_package_data=True,
-    
+
     classifiers=[
     	'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
